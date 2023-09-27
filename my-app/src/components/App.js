cec75de123285edb59e160a0c05618525e16dac9
ShevchenkoDmytriy <dimasevcenko506@gmail.com>
2023-09-27 09:28:30 +0300

    Ready

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 
-class App extends React.Component{
-    render(){
-        return(
-            <div>
-                <Header title="Shapka"/>
-                <Footer />
+class App extends React.Component {
+    render() {
+        return (
+            <div className="Back">
+                <div className="Border">
+<h1>M U L T I P L E X</h1>
+<h3>CINEMA</h3>
+                </div>
             </div>
         )
     }
